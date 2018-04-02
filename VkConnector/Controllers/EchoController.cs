407bbefc9256224484862911b7345d7a0053ac47
banyrule <banyrule@gmail.com>
2018-04-02 23:38:31 +0700

    Переименовал проект

---
+++ VkConnector/Controllers/EchoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VkConnector.Controllers
+{
+    [Route("api/[controller]")]
+    public class EchoController : Controller
+    {
+        [HttpPost]
+        public IActionResult GetReverseRegister([FromBody] string message)
+        {
+            if (message == null)
+            {
+                return BadRequest();
+            }
+            
+            return new ObjectResult(message.ToUpper());
+        }
+    }
+}
+
+
