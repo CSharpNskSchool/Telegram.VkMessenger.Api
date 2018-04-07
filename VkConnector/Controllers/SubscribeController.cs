335e96660531546d1fda5d66c990422d1fcad63c
kamushek <kamushekp@mail.ru>
2018-04-07 15:09:58 +0300

    Собирается

--- VkConnector/Controllers/SubscribeController.cs
+++ VkConnector/Controllers/SubscribeController.cs
@@ -13,18 +13,18 @@ namespace VkConnector.Controllers
         /// <response code="200">WebHook успешно установлен</response>
         /// <response code="400">Установка WebHook не удалась</response>
         [HttpPost]
-        public IActionResult Subscribe([FromBody] SubscribeModel subscribeModel)
+        public IActionResult Subscribe([FromBody] SubscriptionModel subscribeModel)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(new ResponseResult
                 {
-                    Ok = false,
+                    IsOk = false,
                     Description = "Не передан url или данные для авторизации"
                 });
             }
 
-            return BadRequest(new ResponseResult {Ok = false, Description = "Метод еще не реализован"});
+            return BadRequest(new ResponseResult {IsOk = false, Description = "Метод еще не реализован"});
         }
     }
 }
